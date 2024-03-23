ee38bef2f0f97bf6891a0698c68e76d4942b0e57
PinochoBionico1 <pinochobionico@gmail.com>
2024-03-23 01:12:24 +0100

    Almost finished the SQL file - Ignore de .txt file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
 const express = require('express');
+const mysql = require('mysql2');
 const api = express();
 
-api.use(express.static(__dirname + '/public'))
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: null, // Solo para trabajo en local, en caso contrario sustituir por la contraseña del user
+    database: 'hondaAPI'
+});
 
+try {
+    connection.connect(function(){
+
+        $query = "SELECT * FROM Modelos";
+
+        connection.query($query, function(err, rows, fields){
+            if(err){
+                console.log('La consulta no ha salio churra, algo has puesto mal');
+                console.log(err);
+                return
+            } else {
+                console.log('Todo correcto pichita', rows);
+            }
+        });
+    });
+} catch (e) {
+    console.log('Algo a fallado en la conexión notas. Espabila');
+    console.log(e);
+}
+
+api.use(express.static(__dirname + '/public'))
 api.listen(3000, () => {
     console.log('API is up and running on port 3000!');
 });
-
-// api.get('/', (req, res) => {
-//     console.log(req);
-//     res.send('Hello World!');
-// });
