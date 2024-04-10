c1cf0f22f181a8a509ae95a0cb641b94bcb1e3cd
pcabellologic <pcabello@logic10.net>
2024-04-10 17:03:13 +0200

    Limpieza de código

--- index.js
+++ index.js
@@ -67,23 +67,6 @@ db.connect((err) => {
     res.render('GenerationTemplate', { generation_id: generation_id});
   });
 
-
-  app.get('/modelById/:id', (req, res) => { // Función 'modelById': Saca los datos guardados en la bd de un modelo a través de su ID
-    const {id} = req.params;
-    db.query('SELECT * FROM modelos WHERE id = ?', [id], (err, results) => {
-      if (err) throw err;
-      res.json(results[0]);
-    })
-  });
-
-  app.get('/generationsPerModel/:model_id', (req, res) => { // Función 'generationsPerModel': Saca los datos de todas las generaciones asociadas a un modelo
-    const {model_id} = req.params;
-    db.query('SELECT g.* FROM modelos as m INNER JOIN generaciones as g ON m.id = g.modelo_id WHERE m.id = ?;', [model_id], (err, results) => {
-      if (err) throw err;
-      res.json(results);
-    });
-  });
-
   app.get('/generationInfo/:id', (req, res) => { // Función 'generationInfo': Saca los datos de una generación por su ID
     const { id } = req.params;
     db.query('SELECT * FROM generaciones WHERE ID = ?', [id], (err, results) => {
